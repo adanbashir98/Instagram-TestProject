67e25f4d5dd6e88cacb5363903612f7aadaf6288
Adan Bashir <adan.bashir@devsinc.com>
2022-08-21 02:51:10 +0500

    Changes: Story Deletion done, 10 images validation almost done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+  # respond_to :js, :html, :json
+  before_action :authenticate_user!
+  before_action :find_post, only: %i[show destroy edit update]
+
+  def create
+    # @user = User.find(params[:id])
+    # @post = @user.posts.create(post_params)
+    # redirect_to root_path
+  # end
+    @post = current_user.posts.create(post_params)
+    redirect_to root_path
+  end
+
+  def show
+    @posts = Post.all
+  end
+
+  def edit
+    # edit
+  end
+
+  def update
+    # if
+      @post.update(post_params)
+      # render 'edit'
+      redirect_to post_path(@post)
+    # else
+      # redirect_to root_path
+      # render 'edit'
+    # end
+  end
+
+  # def like
+  #   if params[:format] == 'like'
+  #     @post.liked_by current_user
+  #   elsif params[:format] == 'unlike'
+  #     @post.unliked_by current_user
+  #   end
+  # end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    if @post.destroy
+      flash[:notice] = 'Post is deleted!'
+    else
+      flash[:alert] = 'Something went wrong. Try Again!'
+    end
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+    return if @post
+
+    flash[:danger] = 'Post does not exist. Try another post.'
+    redirect_to user_path
+  end
+
+  def post_params
+    params.require(:post).permit(:content, images: [])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,22 @@ class User < ApplicationRecord
 
   enum status: { accessible: 0, inaccessible: 1 }
   validates :email, uniqueness: true
-  validates :username, uniqueness: true, length: { minimum: 5 }, presence: true
-  # :username, uniqueness: true
-  # validates_presence_of :full_name, :username
-  # , :username
+  validates :full_name, presence: true, length: { maximum: 30 }
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :stories, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_one_attached :avatar
+
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :followees, through: :followed_users
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+  has_many :followers, through: :following_users
+  # def self.search(keyword)
+  #   if keyword
+  #     where('name LIKE ?', "%#{keyword}%")
+  #   end
+  # end
+  scope :search, ->(keyword) { where('full_name LIKE ?', "%#{keyword}%") }
 end
