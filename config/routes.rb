c6017134f1c7cd4f14305667b5dfd6911ad6de07
Adan Bashir <adan.bashir@devsinc.com>
2022-08-22 16:51:53 +0500

    Additions: added errors and validations

--- config/routes.rb
+++ config/routes.rb
@@ -23,13 +23,5 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create, :show, :destroy], shallow: true
   end
 
-  # post '/posts/:id/like', to: "likes#create", as: "like_post"
-  # delete '/posts/:id/unlike', to: "likes#destroy", as: "unlike_post"
-
-  # post '/users/:id/follow', to: "users#follow", as: "follow_user"
-  # post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
-
-  # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
