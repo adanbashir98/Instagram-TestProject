4212d2dd0c79b092b15db44e99cf7067b6a77432
Adan Bashir <adan.bashir@devsinc.com>
2022-08-24 13:14:33 +0500

    Seeds File corrected.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-User.create({ full_name: 'Adan Bashir', email: 'adan.bashir@devsinc,com', password: 'adan1234' },
-            { full_name: 'Luke', email: 'luke@devsinc.com', password: 'luke1234' },
-            { full_name: 'Sara', email: 'sara@devsinc.com', password: 'sara1234' })
+User.create({ full_name: 'Adan Bashir', email: 'adan.bashir@devsinc,com', password: 'adan1234' })
+User.create({ full_name: 'Luke', email: 'luke@devsinc.com', password: 'luke1234' })
+User.create({ full_name: 'Sara', email: 'sara@devsinc.com', password: 'sara1234' })
