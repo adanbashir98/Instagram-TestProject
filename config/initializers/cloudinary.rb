4ada5c5f0d163377181f9ccfbdaefbdba02dab26
Adan Bashir <adan.bashir@devsinc.com>
2022-08-24 15:47:52 +0500

    Changed Cloudinary file

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dxmms7rbf'
+  config.api_key = '669217294677112'
+  config.api_secret = 'EIMqlTL51wVjft4N2lWT0_B8WI0'
+  config.secure = true
+end
